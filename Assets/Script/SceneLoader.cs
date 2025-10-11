3a517de0a10c5f2dd16a59d3596f2a651345588a
marikoharuki <150422358+marikoharuki@users.noreply.github.com>
2025-10-11 22:51:47 +0900

    Irisの修復

--- Assets/Script/SceneLoader.cs
+++ Assets/Script/SceneLoader.cs
@@ -1,10 +1,34 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void LoadScene(string name)
+    private float _delayTime = 0f;
+
+    /// <summary>
+    /// ディレイ時間を設定する
+    /// </summary>
+    public void SetDelay(float seconds)
     {
-        SceneManager.LoadScene(name);
+        _delayTime = seconds;
+    }
+
+    /// <summary>
+    /// 指定したシーンをロード（ディレイ付き）
+    /// </summary>
+    public void LoadScene(string sceneName)
+    {
+        StartCoroutine(LoadSceneWithDelay(sceneName));
+    }
+
+    private IEnumerator LoadSceneWithDelay(string sceneName)
+    {
+        if (_delayTime > 0f)
+        {
+            yield return new WaitForSeconds(_delayTime);
+        }
+
+        SceneManager.LoadScene(sceneName);
     }
 }
